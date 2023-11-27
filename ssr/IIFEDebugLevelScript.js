c6087b8e83f5dc97fd72aec540f26ecc9aa1e059
Ed Pike <edpike365@gmail.com>
2023-11-27 19:25:35 +0000

    Added unique key to the script component to suppress react warning.

--- ssr/IIFEDebugLevelScript.js
+++ ssr/IIFEDebugLevelScript.js
@@ -14,6 +14,7 @@ import React from "react";
 const IIFEDebugLevelScript = ({ debugLevelNum }) => {
   return (
     <script
+      key="HSBIIFEDebugLevelScript"
       dangerouslySetInnerHTML={{
         __html: `
             window.__HSBiifeDebugLevel = ${debugLevelNum};
